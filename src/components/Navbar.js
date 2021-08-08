9ffeff7e6d68eae059ffd7bf7baf22626cb3d083
Aukai Tirrell <aukaitirrell@webmocha.com>
2021-08-07 18:14:15 -0700

    update nav and home image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Security Proz Hawaii" style={{ width: 'auto', maxHeight: '100px' }} />
+              <img src={logo} alt="Security Proz Hawaii" style={{ width: 'auto', maxHeight: '100px', paddingBottom: '2px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -59,12 +59,9 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
+            <div className="navbar-end has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
               </Link>
               <Link className="navbar-item" to="/services">
                Services 
@@ -73,7 +70,8 @@ const Navbar = class extends React.Component {
                 Contact Us
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
+            {/* TODO: (at) add integration link here if implemented */}
+            {/* <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -84,7 +82,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </nav>
